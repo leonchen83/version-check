f778374aa9c9a83ff8b4bd797e9b76c3a6a2e12e
leonchen83 <chen.bao.yi@qq.com>
2015-07-19 20:33:25 +0800

    **refactor**  version check.

--- src/main/scala/version/Version.scala
+++ src/main/scala/version/Version.scala
@@ -8,15 +8,7 @@ import scala.util.matching.Regex
 object VersionCheck {
   def latest(vers: Seq[String]): String = {
     vers.maxBy(((r: Regex, str: String) => str match {
-      case r(v1, v2, v3, rc) => (v1.toInt, v2.toInt, Option(v3) match {
-        case Some(v) => v.toInt
-        case None => 0
-      }, Option(rc) match {
-        case Some(v) => v.toInt
-        case None =>
-          // compare tricky
-          Int.MaxValue
-      })
+      case r(v1, v2, v3, rc) => (v1.toInt, v2.toInt, Option(v3).getOrElse("0").toInt, Option(rc).getOrElse(Int.MaxValue.toString).toInt)
       case _ => throw new Exception("parse error")
     })( """(\d+)\.(\d+)(?:\.(\d+))?(?:\-RC(\d+))?""".r, _))
   }
